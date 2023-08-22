2d02240a0e8fe31f74290aa1debbd3962f7ed028
cosminmarina <cosmin.marina@edu.uah.es>
2023-08-22 16:50:10 +0300

    feat: more general inicilizar.py

--- inicializar.py
+++ inicializar.py
@@ -6,7 +6,7 @@ import json
 def inicializar(path:str, size:int=100):
     X1, X2, X3, X5 = (np.random.random(size=(4,size)) > 0.5).astype(np.int8)
     X4 = xor(X2, X3)
-    Y = xor(X1, X4)¡
+    Y = xor(X1, X4)
     df = pd.DataFrame(np.array([X1, X2, X3, X4, X5, Y]).T, columns=["X1", "X2", "X3", "X4", "X5", "Y"])
     df.to_csv(path, index=False)
 
